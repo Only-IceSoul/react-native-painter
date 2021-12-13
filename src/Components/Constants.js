2a3e88fa10e71fbf25ce402071bc3e4b6b52e7bb
Juan <justinjlf21@gmail.com>
2021-12-13 14:06:34 -0400

    ver 2.4.4

---
+++ src/Components/Constants.js
@@ -0,0 +1,118 @@
+
+const Attributes ={
+    mask:'mask',
+    opacity:'opacity',
+
+   //fill stroke 
+   fill: 'fill',
+   fillRule:'fill-rule',
+   fillOpacity: 'fill-opacity',
+   stroke:'stroke',
+   strokeOpacity:'stroke-opacity',
+   strokeWidth:'stroke-width',
+   strokeCap:'stroke-linecap',
+   strokeJoin:'stroke-linejoin',
+   strokeMiter:'stroke-miterlimit',
+
+   transform:'transform',
+
+   //parent ref G
+  
+    curStrokeStart:'curstrokestart',
+    curStrokeEnd:'curstrokeend',
+    curDashArray:'curdasharray',
+    curDashClipValue:'curdashclipvalue',
+    curStroke:'curstroke',
+    curTransform:'curtransform',
+    curX:'curx',
+    curY:'cury',
+    curW:'curw',
+    curH:'curh',
+    curRtl:'currtl',
+    curRtr:'currtr',
+    curRbl:'currbl',
+    curRbr:'currbr',
+    curColors:'curcolors',
+    curPositions:'curpositions',
+    curRadiusX:'curradiusx',
+    curRadiusY:'curradiusy',
+    
+    //shadow
+    shadow:'flood-color',
+    shadowRadius:'stdDeviation',
+    shadowOffsetX:'dx',
+    shadowOffsetY:'dy',
+    shadowRect:{
+        x:'x',
+        y:'y',
+        w:'width',
+        h:'height',
+        units:'filterUnits'
+    },
+
+
+   //others
+   d:'d',
+   filter:'filter',
+   font:'font-family',
+   fontSize:'font-size',
+   fontStyle:'font-style',
+   fontWeight:'font-weight',
+   baseline:'dominant-baseline',
+   textAnchor:'text-anchor',
+   href:'href',
+   x:'x',
+   y:'y',
+   w:'width',
+   h:'height',
+   r:'r',
+   cx:'cx',
+   cy:'cy',
+   rx:'rx',
+   ry:'ry',
+   x1:'x1',
+   x2:'x2',
+   y1:'y1',
+   y2:'y2',
+   startPoint:{
+       x:'x1',
+       y:'y1'
+   },
+   endPoint:{
+    x:'x2',
+    y:'y2'
+},
+gradientTransform:'gradientTransform',
+   strokeDashoffset:'stroke-dashoffset',
+   strokeDasharray:'stroke-dasharray',
+   preserveAspectRatio:'preserveAspectRatio',
+
+
+
+   //elements key
+
+   elements:{
+       prefix:'element',
+       filter:'Filter',
+       shadow:'Shadow',
+       gShadow: 'GStroke',
+       gMask:'GStroke2',
+       fill:'Fill',
+       stroke:'Stroke',
+       mask:'Mask',
+       mask2:'Mask2',
+       strokeMask1:'MaskStroke1',
+       strokeMask2:'MaskStroke2',
+       strokeMask3:'MaskStroke3',
+       strokeMask4:'MaskStroke4',
+       gradient:'Gradient',
+       image:'Image'
+
+   },
+
+
+
+  
+}
+
+export default Attributes
